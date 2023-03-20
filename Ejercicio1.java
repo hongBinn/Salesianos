eb6b9018eac3860ee318de6f24ea69102d4ccb73
Hongbin Ruan <ruan.hon20@zaragoza.salesianos.edu>
2023-03-20 18:48:18 +0100

    Actividad 1 paso 3

---
+++ Ejercicio1.java
@@ -0,0 +1,15 @@
+package tarea1;
+/**
+ * Implementa un programa Java que muestre por pantalla la palabra “Hola” y después,
+ * escriba tu nombre en la siguiente línea.
+ */
+public class Ejercicio1 {
+	/** 
+	 * Pre: ---
+	 * Post: Escribe por pantalla con el mensaje 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola");
+		System.out.println("Hongbin");
+	}
+}
---
+++ Ejercicio2.java
@@ -0,0 +1,17 @@
+package tarea1;
+/**
+ * Implementa un programa Java que dados dos números enteros (x, y)
+ * te muestre por pantalla la suma de ambos.
+ */
+public class Ejercicio2 {
+	/**
+	 * Pre:--
+	 * Post: 
+	 */
+	public static void main (String[] args) {
+		int x=32;
+		int y=543;
+		int suma=x + y;
+		System.out.println(suma);
+	}
+}
